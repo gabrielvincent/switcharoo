b0a2dc1b3843f7042163cb9e4085af5420b36493
Enrico Stemmer <enrico@h3rmt.zip>
2025-04-23 01:48:31 +0200

    chore: nix fmt

--- flake.nix
+++ flake.nix
@@ -1,17 +1,20 @@
 {
   description = "hyprshell - A Rust-based GUI designed to enhance window management in hyprland";
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  outputs = { self, nixpkgs, }: let
-    supportedSystems = ["x86_64-linux" "aarch64-linux"];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    pkgsFor = nixpkgs.legacyPackages;
-  in {
-    packages = forAllSystems (system: rec {
-      hyprshell = pkgsFor.${system}.callPackage ./nix/default.nix {inherit self;};
-      default = hyprshell;
-    });
-    devShells = forAllSystems (system: {
-      default = pkgsFor.${system}.callPackage ./nix/shell.nix {inherit self;};
-    });
-  };
+  outputs = { self, nixpkgs, }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = nixpkgs.legacyPackages;
+    in
+    {
+      packages = forAllSystems (system: rec {
+        hyprshell = pkgsFor.${system}.callPackage ./nix/default.nix { inherit self; };
+        default = hyprshell;
+      });
+      devShells = forAllSystems (system: {
+        default = pkgsFor.${system}.callPackage ./nix/shell.nix { inherit self; };
+      });
+      formatter = forAllSystems (system: pkgsFor.${system}.nixpkgs-fmt);
+    };
 }
