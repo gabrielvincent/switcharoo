35c31a3d7602a6f764598ab1d1f2c095caa39420
Enrico Stemmer <enrico@h3rmt.zip>
2025-07-19 04:38:46 +0200

    chore: working on plugin

--- hyprland-plugin/plugin/main.cpp
+++ hyprland-plugin/plugin/main.cpp
@@ -1,4 +1,5 @@
 #include <hyprland/src/plugins/PluginAPI.hpp>
+#include <hyprland/src/devices/IKeyboard.hpp>
 #include <hyprland/src/includes.hpp>
 
 #include "globals.hpp"
@@ -13,9 +14,43 @@ void onOpenLayerChange(const PHLLS &window) {
     HyprlandAPI::addNotification(PHANDLE, "AMONGUS", CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
 }
 
+void onKeyPressTest(const std::unordered_map<std::string, std::any> &data) {
+    std::string thit = "data: " + std::to_string(data.size());
+    auto keyboardIt = data.find("keyboard");
+    auto eventIt = data.find("event");
+
+    if (keyboardIt != data.end() && eventIt != data.end()) {
+        auto keyboard = std::any_cast<CSharedPointer<IKeyboard> >(keyboardIt->second);
+        auto event = std::any_cast<IKeyboard::SKeyEvent>(eventIt->second);
+        auto ab = std::to_string(event.keycode);
+        HyprlandAPI::addNotification(PHANDLE, ab, CHyprColor{0.2, 1.0, 0.2, 1.0}, 5000);
+    }
+}
+
+void onKeyPress(const std::unordered_map<std::string, std::any> &data) {
+    auto keyboardIt = data.find("keyboard");
+    auto eventIt = data.find("event");
+
+    if (keyboardIt != data.end() && eventIt != data.end()) {
+        auto keyboard = std::any_cast<CSharedPointer<IKeyboard> >(keyboardIt->second);
+        auto event = std::any_cast<IKeyboard::SKeyEvent>(eventIt->second);
+
+        if (event.keycode == HYPRSHELL_OVERVIEW_MOD_KEYCODE) {
+            if (event.state == WL_KEYBOARD_KEY_STATE_PRESSED) {
+                HyprlandAPI::addNotification(PHANDLE, "Overview key pressed", CHyprColor{0.2, 1.0, 0.2, 1.0}, 5000);
+            } else if (event.state == WL_KEYBOARD_KEY_STATE_RELEASED) {
+                HyprlandAPI::addNotification(PHANDLE, "Overview key released", CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
+            }
+        }
+    }
+}
+
 APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     PHANDLE = handle;
-    HyprlandAPI::addNotification(PHANDLE, "---AMONGUS----", CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
+
+    if constexpr (HYPRSHELL_PRINT_START == 1) {
+        HyprlandAPI::addNotification(PHANDLE, "Hyprshell Plugin started", CHyprColor{0.2, 1.0, 0.2, 1.0}, 5000);
+    }
 
     // ALWAYS add this to your plugins. It will prevent random crashes coming from
     // mismatched header versions.
@@ -26,7 +61,8 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     }
 
     // clang-format off
-    static auto P = HyprlandAPI::registerCallbackDynamic(PHANDLE, "openLayer",[&](void* self, SCallbackInfo& info, std::any data) { onOpenLayerChange(std::any_cast<PHLLS>(data)); });
+    static auto P1 = HyprlandAPI::registerCallbackDynamic(PHANDLE, "openLayer",[&](void*, SCallbackInfo&, const std::any &data) { onOpenLayerChange(std::any_cast<PHLLS>(data)); });
+    static auto P2 = HyprlandAPI::registerCallbackDynamic(PHANDLE, "keyPress",[&](void*, SCallbackInfo&, const std::any &data) { onKeyPress(std::any_cast<std::unordered_map<std::string, std::any>>(data)); });
     // clang-format on
 
     const std::string name = HYPRSHELL_PLUGIN_NAME;
--- hyprland-plugin/src/configure.rs
+++ hyprland-plugin/src/configure.rs
@@ -6,6 +6,12 @@ use std::path::Path;
 use tempfile::TempDir;
 use tracing::{Level, span};
 
+struct Config {
+    overview_key: Option<String>,
+    overview_mod: Option<String>,
+    switch_mod: Option<String>,
+}
+
 pub fn configure(dir: &TempDir) -> anyhow::Result<()> {
     let _span = span!(Level::TRACE, "configure", path =? dir.path()).entered();
     let defs = dir.path().join("defs.hpp");
@@ -19,10 +25,17 @@ pub fn configure(dir: &TempDir) -> anyhow::Result<()> {
         .read_to_string(&mut buffer)
         .context("unable to read defs file")?;
     for replace in [
-        ("@HYPRSHELL_PLUGIN_NAME@", PLUGIN_NAME),
-        ("@HYPRSHELL_PLUGIN_AUTHOR@", PLUGIN_AUTHOR),
-        ("@HYPRSHELL_PLUGIN_DESC@", PLUGIN_DESC),
-        ("@HYPRSHELL_PLUGIN_VERSION@", PLUGIN_VERSION),
+        ("#include \"defs-test.hpp\"", ""),
+        ("_HYPRSHELL_PLUGIN_NAME_", PLUGIN_NAME),
+        ("_HYPRSHELL_PLUGIN_AUTHOR_", PLUGIN_AUTHOR),
+        ("_HYPRSHELL_PLUGIN_DESC_", PLUGIN_DESC),
+        ("_HYPRSHELL_PLUGIN_VERSION_", PLUGIN_VERSION),
+        (
+            "_HYPRSHELL_PRINT_START_",
+            if cfg!(debug_assertions) { "1" } else { "0" },
+        ),
+        ("_HYPRSHELL_OVERVIEW_MOD_KEYCODE_", "125"),
+        ("_HYPRSHELL_OVERVIEW_KEY_KEYCODE_", "15"),
     ] {
         buffer = buffer.replace(replace.0, replace.1);
     }
