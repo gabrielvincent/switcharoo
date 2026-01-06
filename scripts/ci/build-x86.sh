eada9385f52846c6468891cb061c5a879fd16098
Enrico Stemmer <enrico@h3rmt.zip>
2026-01-06 03:02:28 +0100

    fix: fix release ci workflow
    Signed-off-by: Enrico Stemmer <enrico@h3rmt.zip>

--- scripts/ci/build-x86.sh
+++ scripts/ci/build-x86.sh
@@ -11,5 +11,5 @@ ls -lh /tmp/hyprshell-x86_64.tar.zst
 cargo build --release --target x86_64-unknown-linux-gnu --no-default-features --features slim
 ls -lh target/x86_64-unknown-linux-gnu/release/hyprshell
 
-tar --zstd -cf /tmp/hyprshell-x86_64-slim.tar.zst --transform 's,.*/,,' LICENSE README.md docs/CONFIGURE.md docs/DEBUG.md target/x86_64-unknown-linux-gnu/release/hyprshell packaging/hyprshell.service packaging/hyprshell-settings.png packaging/hyprshell-settings.desktop packaging/usr-share.tar
-ls -lh /tmp/hyprshell-x86_64-slim.tar.zst
+tar --zstd -cf /tmp/hyprshell-slim-x86_64.tar.zst --transform 's,.*/,,' LICENSE README.md docs/CONFIGURE.md docs/DEBUG.md target/x86_64-unknown-linux-gnu/release/hyprshell packaging/hyprshell.service packaging/hyprshell-settings.png packaging/hyprshell-settings.desktop packaging/usr-share.tar
+ls -lh /tmp/hyprshell-slim-x86_64.tar.zst
